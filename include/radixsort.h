74b29ba27184099428a82ab61a919cfb0d52bcf6
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-02 22:05:48 -0700

    not sure why not working

---
+++ include/radixsort.h
@@ -0,0 +1,34 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex05-radixsort
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
+
+#ifndef EX05_RADIXSORT_RADIXSORT_H
+#define EX05_RADIXSORT_RADIXSORT_H
+
+#include <cstdint>
+#include <iostream>
+
+namespace edu {
+    namespace vcccd {
+        namespace vc {
+            namespace csv15 {
+
+                template<typename>
+                void radixsort(T array[], size_t size){
+                    std::cout << "Hello!" <<std::endl;
+                }
+            }
+        }
+    }
+}
+
+#endif //EX05_RADIXSORT_RADIXSORT_H
---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex03-fibonacci
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
+#include <iostream>
+#include "radixsort.h"
+using edu::vcccd::vc::csv15::radixsort;
+
+int main(int argc, char *argv[]){
+    radixsort();
+}
