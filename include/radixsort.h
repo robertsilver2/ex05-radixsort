748c8c13f7ef735e2a69b58068e1b7f58c2a0369
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-21 01:54:00 -0800

    staying current --working on dynamic multidimensional array allocation

--- include/radixsort.h
+++ include/radixsort.h
@@ -24,10 +24,35 @@ namespace edu {
 
                 template<typename T>
                 void radixsort(T array[], size_t size){
-                    //DEBUG: print array
+                    //DEBUG: print unsorted input array
+                    std::cout << "unsorted:" << std::endl;
                     for(int i = 0; i<size; i++){
                         std::cout << "[" << array[i] << "]" << std::endl;
                     }
+                    //Following: multi dimensional array info from:
+                    //http://www.cplusplus.com/doc/tutorial/arrays/
+
+                    /* notes on dynamic array allocation
+                     *--single dimension:
+                     *http://www.fredosaurus.com/notes-cpp/newdelete/50dynamalloc.html
+                     *--2d:
+                     * https://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-in-c-using-new/936702#936702
+                     *Look at answer by Mehrdad Afshari
+                     */
+
+                    size_t width = 10;
+                    size_t height = size;
+
+                    //DEBUG: print buckets, no values given
+                    std::cout << "buckets, empty:" << std::endl;
+                    for(int i = 0; i<width; i++){
+                        for(int j = 0; j<height; j++) {
+                            std::cout << " [" << buckets[i][j] << "] ";
+                        }
+                        std::cout << std::endl;
+                        std::cout << std::endl;
+                    }
+
 
 
                 }
--- src/main.cc
+++ src/main.cc
@@ -17,6 +17,6 @@ using edu::vcccd::vc::csv15::radixsort;
 int main(int argc, char *argv[]){
     uint64_t array[] = {1819, 778, 1761, 471, 65, 604, 302, 1300, 299, 1603, 202};
     //array generated @ https://www.random.org/integer-sets/
-    size_t size = 25;
+    size_t size = 11;
     radixsort(array, size);
 }
